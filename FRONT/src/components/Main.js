7a890b481a0618a1b964bc77392463968f99f6f4
jomim2 <wjddla7374@naver.com>
2025-01-05 18:00:09 +0900

    hi

--- FRONT/src/components/Main.js
+++ FRONT/src/components/Main.js
@@ -1,15 +1,34 @@
-import React from "react";
+import React, { useEffect, useState, useRef } from 'react';
 import styles from '../css/main.module.css';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrashCan } from '@fortawesome/free-solid-svg-icons';
 const Main = () => {
+    const [text, setText] = useState("");
+    const throwBtnClick = async () => {
+        try {
+            const res = await fetch("http://127.0.0.1:8000/consult/", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    consult: text
+                }),
+            });
+            console.log("test OK");
+            const jsonData = await res.json();
+            console.log(jsonData);
+        } catch (err) {
+            console.log(err);
+        }
+    }
     return (
         <main className={styles.mainArea}>
             <div className={styles.timeArea}>24년 1월 04일</div>
             <div className={styles.textBoxesArea}>
                 <div className={`${styles.leftTextBox} ${styles.textBox}`}>
-                    <textarea className={styles.textArea}></textarea>
-                    <div className={`${styles.throwBtn} ${styles.btns}`}>버리기</div>
+                    <textarea className={styles.textArea} value={text} onChange={(e) => { setText(e.target.value) }}></textarea>
+                    <div onClick={throwBtnClick} className={`${styles.throwBtn} ${styles.btns}`}>버리기</div>
                 </div>
                 <div className={`${styles.rightTextBox} ${styles.textBox}`}>
                     <textarea className={styles.textArea}></textarea>
