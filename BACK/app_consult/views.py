e410e01fb6f83d11a10db907b1af814b2b0cc16b
Peterseo9503 <tjehdgus9503@gmail.com>
2025-01-05 18:59:59 +0900

    changed backend

--- BACK/app_consult/views.py
+++ BACK/app_consult/views.py
@@ -1,16 +1,20 @@
 from django.http import JsonResponse
+from django.http import HttpResponse
+
 from django.views.decorators.csrf import csrf_exempt
 
 import os
 import openai
 
 import json
+
 @csrf_exempt
 def consult(req):
-    
+    print("Received request:", req.method) 
     if req.method == "POST":
         reqDate = json.loads(req.body.decode('utf-8'))
-        print(reqDate.consult)
+        print(req)
+        print(reqDate["consult"])
 
         openai.api_key = os.getenv("OPENAI_API_KEY").strip()
         prompt = """너는 우울한 사람들 상담하는 심리 상담사야. 상담내용을 긍정적인 내용으로 바꾸면 돼. 혹시 장원영의 럭키비키식 사고 알고 있으면 그렇게 해.
@@ -19,10 +23,12 @@ def consult(req):
             model="gpt-3.5-turbo",
             messages=[
                 {"role": "system", "content": prompt},
-                {"role": "user", "content": reqDate.consult}
+                {"role": "user", "content": reqDate["consult"]}
             ]
         )
         print(response['choices'][0]['message']['content'])
         consult = response['choices'][0]['message']['content']
         
         return JsonResponse( { "consult": consult } )
+        # return JsonResponse({"consult":"test"})
+    return HttpResponse("test")
