08abcff943e982784ed73213ca152a1738612313
jomim2 <wjddla7374@naver.com>
2025-01-05 19:01:40 +0900

    hi3

--- FRONT/src/components/Main.js
+++ FRONT/src/components/Main.js
@@ -4,9 +4,16 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrashCan } from '@fortawesome/free-solid-svg-icons';
 const Main = () => {
     const [text, setText] = useState("");
+    const [gptValue , setGptValue] = useState("");
+    const today = new Date();
+    const displayToday = today.toISOString().slice(0,10);
+    const handleDrop = (e) =>{
+        e.preventDefault();
+        throwBtnClick();
+    }
     const throwBtnClick = async () => {
         try {
-            const res = await fetch("http://127.0.0.1:8000/consult/", {
+            const res = await fetch("http://localhost:8000/consult/", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
@@ -18,25 +25,26 @@ const Main = () => {
             console.log("test OK");
             const jsonData = await res.json();
             console.log(jsonData);
+            setGptValue(jsonData);
         } catch (err) {
             console.log(err);
         }
     }
     return (
         <main className={styles.mainArea}>
-            <div className={styles.timeArea}>24년 1월 04일</div>
+            <div className={styles.timeArea}>{displayToday}</div>
             <div className={styles.textBoxesArea}>
                 <div className={`${styles.leftTextBox} ${styles.textBox}`}>
-                    <textarea className={styles.textArea} value={text} onChange={(e) => { setText(e.target.value) }}></textarea>
+                    <textarea className={styles.textArea} value={text} onChange={(e) => { setText(e.target.value) }} draggable="true"></textarea>
                     <div onClick={throwBtnClick} className={`${styles.throwBtn} ${styles.btns}`}>버리기</div>
                 </div>
                 <div className={`${styles.rightTextBox} ${styles.textBox}`}>
-                    <textarea className={styles.textArea}></textarea>
+                    <textarea className={styles.textArea}>{gptValue}</textarea>
                     <div className={`${styles.againBtn} ${styles.btns}`}>다시쓰기</div>
                 </div>
             </div>
             <footer className={styles.trashCanArea}>
-                <FontAwesomeIcon icon={faTrashCan} className={styles.trashCan} />
+                <FontAwesomeIcon icon={faTrashCan} className={styles.trashCan} onClick={throwBtnClick} />
             </footer>
         </main>
     )
